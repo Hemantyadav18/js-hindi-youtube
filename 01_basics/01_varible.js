451d3b56444b8821940076f1f82338a7311a21ee
Hemantyadav18 <144705630+Hemantyadav18@users.noreply.github.com>
2024-07-12 16:37:51 +0000

    datatypes

---
+++ 01_basics/01_varible.js
@@ -0,0 +1,15 @@
+const accountid=144553
+let accountEmail="hemanty4472@gmail.com"
+var accountpassword="12345"
+accountcity="indore"
+let accountstate;
+
+//accountid=2   //not allowed
+accountEmail="hy@18"
+accountpassword="1813"
+accountcity="khargone"
+console.log(accountid)
+/*
+perfer not to use var
+*/
+console.table({accountid,accountEmail,accountpassword,accountcity,accountstate})
