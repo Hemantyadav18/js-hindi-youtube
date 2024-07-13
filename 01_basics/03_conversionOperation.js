d6bb8c941751dc462019a9889bc0bb82038f5d24
Hemantyadav18 <144705630+Hemantyadav18@users.noreply.github.com>
2024-07-13 16:50:24 +0000

    Conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,23 @@
+let score="hemant"
+//console.log(typeof score);
+
+let valueInNumber=Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);  
+
+//"33"=>33
+//"33abc"=>NaN
+//true=>1; false=>0
+
+let isLoggedin="hemant"
+let booleanIsLoggedin=Boolean(isLoggedin)
+console.log(booleanIsLoggedin);
+
+//1=>true; 0=>false
+//"" =>false
+//"hemant"=>true
+
+let someNumber=33
+let stringNumber=String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
