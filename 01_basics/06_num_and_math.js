5c3c59ddaad0179ca94b32a0a9609a3bdfeb4f3a
Hemantyadav18 <144705630+Hemantyadav18@users.noreply.github.com>
2024-07-14 13:54:21 +0000

    num and maths

---
+++ 01_basics/06_num_and_math.js
@@ -0,0 +1,40 @@
+const score=400
+
+const balence=new Number(100)
+//console.log(balence);
+
+//console.log(balence.toString().length);
+//console.log(balence.toFixed(1));
+
+
+const otherNumber=123.8966
+
+//console.log(otherNumber.toPrecision(4));
+
+const hundread=1000000
+//console.log(hundread.toLocaleString('en-IN'));
+
+// ******************************************* Maths **************************************************
+
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.3));
+// console.log(Math.ceil(4.3));
+// console.log(Math.floor(4.9));
+// console.log(Math.sqrt(25));
+// console.log(Math.sqrt(1000));
+// console.log(Math.min(4,3 ,5,6));
+// console.log(Math.max(4,3,5,6));
+
+
+console.log(Math.random());
+console.log(Math.random()*10 + 1);
+console.log(Math.floor(Math.random()*10) + 1);
+
+const min=10
+const max=20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
+
+
