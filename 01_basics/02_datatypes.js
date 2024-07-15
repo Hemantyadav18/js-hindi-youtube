56d102b8193a51bc62b45bee830e47b9a0579dfd
Hemantyadav18 <144705630+Hemantyadav18@users.noreply.github.com>
2024-07-15 16:37:15 +0000

    datatypes in js

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,25 @@
+"use strict"  //treat all js code as newer version
+//alert(3+3) //we are using node js,not browser
+
+console.log(3
+    +
+    
+    3)  //code readulibty should be high
+
+    console.log("Hemant")
+
+    let name="hemant"
+    let age=21
+    let isLoggedin=false
+
+    //number => 2 to power 53
+    //bigint
+    //string=> ""
+    //boolean => true/false
+    //null =>standalone value
+    //symbol =>unique
+    //object
+
+    console.log(typeof age);
+    console.log(typeof null) ;//object
+    console.log(typeof undefined);//undefined
