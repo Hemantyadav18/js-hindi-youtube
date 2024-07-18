1a886a98e0d42a06c5d48db2db334e3b06b7289b
Hemantyadav18 <144705630+Hemantyadav18@users.noreply.github.com>
2024-07-18 15:49:31 +0000

    Object in js

--- 02_basics/04_Object.js
+++ 02_basics/04_Object.js
@@ -44,11 +44,30 @@ const users=[
    
 ]
 
-console.log(users[2].name);
-console.log(tinderUser);
+// console.log(users[2].name);
+// console.log(tinderUser);
 
-console.log(Object.keys(tinderUser));
-console.log(Object.values(tinderUser));
-console.log(Object.entries(tinderUser));
+// console.log(Object.keys(tinderUser));
+// console.log(Object.values(tinderUser));
+// console.log(Object.entries(tinderUser));
 
-console.log(tinderUser.hasOwnProperty('isLogged'));
+// console.log(tinderUser.hasOwnProperty('isLogged'));
+
+
+
+const course={
+    coursename:"js in hindi",
+    price:"999",
+    courseInstructor:"hemant"
+}
+
+//console.log(course.courseInstructor);
+const{courseInstructor:instructor}=course
+console.log(instructor);
+
+// {
+//     "name":"hemant",
+//     "coursename":"js in hindi",
+//     "price":"free"
+// }
+
