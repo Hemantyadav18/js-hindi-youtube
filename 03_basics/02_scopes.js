5f49164b24c6f09dfc55369c707ac8f3d02910e0
Hemantyadav18 <144705630+Hemantyadav18@users.noreply.github.com>
2024-07-21 12:39:19 +0000

    scope in js

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -4,8 +4,46 @@ let a=300
 if(true){
     let a=10
     const b=20
-    console.log("INNer:",a);
+    //console.log("INNer:",a);
 }
-console.log(a);
+//console.log(a);
 //console.log(b);
 //console.log(c);
+
+function one(){
+    const username="hemant"
+     
+    function two(){
+        const website="youtube"
+       // console.log(username);
+
+    }
+  //  console.log(website);
+
+  //two()
+}
+//one()
+
+if(true){
+    const username="hemant";
+    if(username==="hemant"){
+        const website="youtube";
+       // console.log(username+website);
+    }
+    // console.log(website);
+}
+//console.log(username);
+
+// +++++++++++++++++++++++++++ interesting +++++++++++++++++++++
+
+console.log(addone(5));
+function addone(num){
+    return num+1
+}
+
+console.log(addTwo(5));
+const addTwo=function(num){
+    return num+2
+}
+
+
