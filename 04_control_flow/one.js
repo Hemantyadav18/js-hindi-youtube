1b936dc9e5e2916e61dd2d1bbc932f271197e0c8
Hemantyadav18 <144705630+Hemantyadav18@users.noreply.github.com>
2024-07-24 14:53:18 +0000

    control flow in js

---
+++ 04_control_flow/one.js
@@ -0,0 +1,37 @@
+//if
+
+// const isUserloggedIn=true;
+// if(2==="2"){
+//     console.log("executed");
+// }
+
+// const temperature=41;
+// if(temperature===41){
+//     console.log("less than 50");
+// }else{
+//     console.log("temperature is greater than 50");
+// }
+
+//<, >,<=,>=,== , !=, ===, !==
+
+
+// const score=200
+// if(score>100){
+//     let power="fly"
+//     console.log(`user power: ${power}`);
+// }
+// console.log(`user power: ${power}`);
+
+
+// const isUserloggedIn=true;
+// const debitCard=true;
+// if(isUserloggedIn && debitCard && 2==2){
+//     console.log("allow to buy course");
+// }
+
+const loggedInFromGoogle=true;
+const loggedInFromEmail=false;
+
+if(loggedInFromEmail || loggedInFromGoogle ){
+    console.log("user logged in ");
+}
